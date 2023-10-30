d8005589a69f1921b12814982fbf4580645e366a
Nguyenduc2108 <vanducnguyen2108@gmail.com>
2023-10-30 16:55:01 +0700

    btvn 40

--- btvn/btvn_40/btvn40/src/Pages/ProductDetail.js
+++ btvn/btvn_40/btvn40/src/Pages/ProductDetail.js
@@ -1,12 +1,8 @@
 export const ProductDetail = ({ data }) => {
   const id = data.id;
 
-  const navigate = () => {
-    window.navigate("/san-pham");
-  };
-
   return `
   <h1>Chi tiết sản phẩm: ${id}</h1>
-  <button onclick="navigate()">Back</button>
+  <button onclick= "navigate('/san-pham')" >Back</button>
   `;
 };
--- btvn/btvn_40/btvn40/src/Utils/router.js
+++ btvn/btvn_40/btvn40/src/Utils/router.js
@@ -1,7 +1,9 @@
 import Navigo from "navigo";
 import { Error } from "../Error";
 
-const Router = new Navigo("/", { linksSelector: "a" });
+const Router = new Navigo("/f8-fullstack-k3/btvn/btvn_40/btvn40/dist", {
+  linksSelector: "a",
+});
 const app = document.querySelector("#app");
 
 export const router = (pathList = [], DefaultLayout) => {
